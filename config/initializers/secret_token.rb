c4235cc3dd751937fa85552df64ae4d723d5d990
horacix <hgonzalezl@gmail.com>
2011-03-28 11:31:44 -0700

    Inicializacion de app de Fondos Mutuos, con una primera version de modelo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fm::Application.config.secret_token = '89ec841905eeb5131f9ee8adbde145225254ade71abc7d9665ccdeaa26a784702b278d3f1370b43754c3e192b2f324519f61e46d37b171d90f0750ce581bfc0c'
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+Administrator.create(:id => 96767630, :name => 'BANCHILE')
+Administrator.create(:id => 96514410, :name => 'BICE')
+Administrator.create(:id => 96667040, :name => 'SANTANDER ASSET MANAGEMENT')
+Administrator.create(:id => 91999000, :name => 'PRINCIPAL')
